543d7ede40e2d3e2b4852a015810eee072ba7f04
thejashc <thejashc@gmail.com>
2017-11-21 14:07:29 +0100

    post processing file with analytical soln

---
+++ ODE/post_processing.m
@@ -0,0 +1,122 @@
+function [] = post_processing(x,v, t)
+
+%Plotting energies, positions, velocities and phase space trajectories
+global x0 v0 omg0 k m friction gamma forcing fmag omg
+
+if (friction == 0 && forcing == 0)
+    
+    % analytical solutions for position and velocity
+    x_anal =  x0*cos(omg0*t)     + (v0/omg0)*sin(omg0*t);
+    v_anal = -x0*omg0*sin(omg0*t) + v0*cos(omg0*t);
+    
+    % position
+    figure(1)
+    ind = 1:10:length(t);
+    x_anal_handpick = x_anal(ind);
+    hold on
+    plot(t(ind),x(ind),'ko');
+    plot(t,x_anal,'r.');
+    plot(t(ind), x_anal_handpick, 'ko','MarkerFaceColor','k')
+    
+    % velocity
+    figure(2)
+    v_anal_handpick = v_anal(ind);
+    hold on
+    plot(t(ind),v(ind),'k^','MarkerSize',3.5);
+    plot(t,v_anal,'r^','MarkerSize',3.5);
+    plot(t(ind), v_anal_handpick, 'k^','MarkerSize',3.5,'MarkerFaceColor','k')
+    
+    % energy
+    E_init = 0.5*(k*x0^2 + m*v0^2)*ones(length(t),1);
+    pot  = 0.5 * k * x.^2;
+    kin  = 0.5 * m * v.^2;
+    tot = pot + kin;
+    figure(3)
+    hold on
+    plot(t,pot);
+    plot(t,kin);
+    plot(t,tot);
+    plot(t,E_init)
+    legend('Potential','Kinetic','Total','Initial')
+    
+    % plotting the phase-space plot for the spring mass system
+    figure(4)
+    a = sqrt((v0/omg0)^2 + (x0)^2);
+    b = sqrt(v0^2 + (x0*omg0)^2);
+    syms theta
+    x_ellipse = a*cos(theta);
+    y_ellipse = b*sin(theta);
+    scatter(x, v, 'b.')
+    axis square
+    hold on;
+    fplot(x_ellipse,y_ellipse)
+    
+elseif (friction == 1 && forcing == 0)
+    %% post processing with friction
+    % analytical solutions for position and velocity
+    alpha = gamma/(2*m);
+    omg_new = sqrt(omg0^2 - alpha^2);
+    x_anal = (x0*cos(omg_new*t)     + (v0/omg_new)*sin(omg_new*t)).*exp(-alpha*t);
+    v_anal = (-x0*omg_new*sin(omg_new*t) + v0*cos(omg_new*t)).*exp(-alpha*t) -alpha*exp(-alpha*t).*(x0*cos(omg_new*t)+(v0/omg_new)*sin(omg_new*t));
+    
+    % position
+    figure(1)
+    plot(t, x_anal, 'r', 'LineWidth', 2)
+    hold on;
+    plot(t,x,'k.');
+    
+    % velocity
+    figure(2)
+    plot(t,v_anal, 'r', 'LineWidth', 2)
+    hold on;
+    plot(t,v,'k.');
+    
+    % energy
+    E_init = 0.5*(k*x0^2 + m*v0^2);
+    pot  = 0.5 * k * x.^2;
+    kin  = 0.5 * m * v.^2;
+    tot = pot + kin;
+    figure(3)
+    plot(t, tot)
+    hold on;
+    plot(t, E_init*exp(-(gamma/m)*t))
+    
+elseif (friction == 0 && forcing == 1)
+    %% post processing with forcing
+    % analytical solutions for position and velocity
+    tan_phi = (-v0/omg0)/(x0 - (fmag/m)*(1/(omg0^2 - omg^2)));
+    phi = atan(tan_phi);
+    B = -(v0/omg0)*(1/sin(phi));
+    x_anal = (fmag/m)*(1/(omg0^2 - omg^2))*cos(omg*t) + B*cos(omg0*t + phi);
+    
+    % position
+    figure(1)
+    plot(t,x,'k.');
+    hold on;
+    plot(t,x_anal, 'r')
+    
+    % velocity
+    figure(2)
+    plot(t,v,'k.');
+    hold on;
+    
+elseif (friction == 1 && forcing == 1)
+    %% post processing with both friction and forcing
+    % analytical solutions for position and velocity
+    phi = atan((gamma/m)*omg/(omg0^2 - omg^2));
+    x_anal = (fmag/m)*(1/( (omg0^2 - omg^2)^2 + (omg*(gamma/m))^2 )^(1/2))*cos(omg*t - phi);
+    
+    % position
+    figure(1)
+    plot(t,x,'k.');
+    hold on;
+    plot(t,x_anal, 'r')
+    
+    % velocity
+    figure(2)
+    plot(t,v,'k.');
+    hold on;
+end
+
+end
+
